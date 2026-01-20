05ec84827ef85270bf0df8ee2181df50028590df
irfanfadhli <irfanfadhlim@gmail.com>
2026-01-20 13:41:07 +0700

    update

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { bookingService } from '@/lib/services'
+import { bookingService, roomService } from '@/lib/services'
 import { Room, Booking } from '@/lib/types'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
